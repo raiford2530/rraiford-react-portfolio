26045f6bf3debabdd187a6d05ef7d84f8f02921d
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:44:18 -0500

    Added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+function Footer(){
+    return (
+        <div className="footer d-flex align-items-center justify-content-center">
+            <span>Copyright &copy; 2021 Rashawn Raiford</span>
+            
+        </div>
+    )
+}
+
+export default Footer;
