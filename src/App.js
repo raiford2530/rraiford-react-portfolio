b46c8b1a61316a72811622b5eb078fc22b0ab0d7
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:47:46 -0500

    Added styling for app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import About from './containers/About/About';
+import Portfolio from './containers/Portfolio/Portfolio'
+import Contact from './containers/Contact/Contact'
+import ProjectDetails from './containers/ProjectDetails/ProjectDetails'
+import Footer from './components/Footer/Footer';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="root-container">
+      <Navbar/>   
+      <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route exact path="/about">
+        <About/>
+      </Route>
+      <Route exact path="/portfolio">
+        <Portfolio/>
+      </Route> 
+      <Route exact path="/contact">
+        <Contact/>
+      </Route> 
+      <Route  path="/projects/:name">
+        <ProjectDetails/>
+      </Route>    
+      </Switch>     
+      <Footer/>
     </div>
+    </Router>
   );
 }
 
