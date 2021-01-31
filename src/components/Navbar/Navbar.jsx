baf0385abfe38046ca53e369fb9dad287402f337
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:44:54 -0500

    Added navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <Link to="/portfolio" className="nav-link">
+                    Portolio
+                  </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-link">About Me</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/contact" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
+}
+
+export default Navbar;
