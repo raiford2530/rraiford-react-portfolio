b40dab28bd7e6e29ab10457e8da73b235e4f1104
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:45:33 -0500

    Added about component

---
+++ src/containers/About/About.jsx
@@ -0,0 +1,38 @@
+import ContentContainer from '../ContentContainer/ContentContainer';
+import Header from "../../components/Header/Header";
+import Headshot from '../../assets/images/headshot.jpg';
+import './about.css';
+
+function About() {
+  return (
+    <ContentContainer>
+      <Header title="About" />
+    <div className="d-flex justify-content-center">
+        <img id="headshot" src={Headshot} alt="Photo of Rashawn"/>
+    </div>
+     
+      <p>
+        Hi, I'm Rashawn Raiford. Ever since I was a teenager, I have been
+        fascinated with computers and taking on challenges. Computers were my
+        way of escaping from my small, rural hometown to see places I had never
+        seen and to discover new things.
+      </p>
+      <p>
+        After high school, I started college at Augusta State University. I
+        graduated with a Bachelor's in Management Information Systems in 2012,
+        but after taking a few programming classes during my junior and senior
+        year I became fascinated with computer programming. After I graduated, I
+        started engaging in books and taking online courses to learn more about
+        programming
+      </p>
+      <p>
+        After I became more confident in my programming skills, I began my
+        journey to start my software development career. I have been employed as
+        a software developer for five years now and have done both frontend and
+        backend work on multiple projects.
+      </p>
+    </ContentContainer>
+  );
+}
+
+export default About;
