a381840fdbabdd7af5e9a3fd82268b89850006e5
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:45:07 -0500

    Added project component

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,30 @@
+import './project.css';
+import {Link} from 'react-router-dom';
+
+function Project(props) {
+    const projectLinkProps = {
+        pathname: `/projects/${props.title}`,
+        state: {
+            title: props.title,
+            description: props.description,
+            image: props.image,
+            details: props.details
+        }
+    }
+
+  return (
+    <Link to={projectLinkProps} style={{textDecoration:'none', color:'white'}}>
+    <div className="card bg-project">
+      <img src={props.thumbnail} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">
+          {props.description}.
+        </p>
+      </div>
+    </div>
+    </Link>
+  );
+}
+
+export default Project;
