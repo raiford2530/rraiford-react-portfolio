cae5a6dd72d7ba3d68c97d40173c89977cf5041a
Rashawn Raiford <raiford87@gmail.com>
2021-01-30 23:46:38 -0500

    Added home component

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,25 @@
+import Sphere from "../../assets/images/spinning-sphere.gif";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="main-container">
+      <div className="container h-100 d-flex align-items-center">
+        <div className="row mx-auto">
+          <div className="col-sm-12">
+            <h1>
+              Hi, I'm Rashawn
+              <br />
+              I'm a Full Stack Developer.
+            </h1>
+            <div className="d-flex justify-content-center mt-5">
+                <img className="sphere" src={Sphere}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
